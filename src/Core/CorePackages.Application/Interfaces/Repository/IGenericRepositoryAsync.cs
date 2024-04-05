7dfa1e68cb479da493ec9149d42f60a6659136a0
Tunahan <Feleket@DESKTOP-E6PUBRK>
2024-04-06 01:35:27 +0300

    Core layer

---
+++ src/Core/CorePackages.Application/Interfaces/Repository/IGenericRepositoryAsync.cs
@@ -0,0 +1,10 @@
+﻿using CorePackages.Domain.Comman;
+
+namespace CorePackages.Application.Interfaces.Repository;
+
+public interface IGenericRepositoryAsync<T> where T : BaseEntity
+{
+    Task<List<T>> GetAll();
+    Task<T> GetById(Guid Id);
+    Task<T> GetByDynamic();
+}
--- src/Core/CorePackages.Domain/Entities/Configuration.cs
+++ src/Core/CorePackages.Domain/Entities/Configuration.cs
@@ -2,34 +2,12 @@
 
 namespace CorePackages.Domain.Entities;
 
-public class Configuration<T> : BaseEntity
+public class Configuration : BaseEntity
 {
     public string Name { get; set; }
-    // Type alanını T tipine göre otomatik olarak belirlemek için
-    public string Type
-    {
-        get
-        {
-            return GetTypeString(typeof(T));
-        }
-    }
-    public T Value { get; set; }
+    public string Type { get; set; }
+    public string Value { get; set; }
     public bool IsActive { get; set; }
     public string ApplicationName { get; set; }
 
-
-
-    private string GetTypeString(Type type)
-    {
-        if (type == typeof(int))
-            return "int";
-        else if (type == typeof(string))
-            return "string";
-        else if (type == typeof(double))
-            return "double";
-        else if (type == typeof(bool))
-            return "bool";
-        else
-            return "unknown";
-    }
 }
