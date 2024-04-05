86483ca5a76037cb5bad2a87666c42922953dc87
Feleket <Feleket@DESKTOP-E6PUBRK>
2024-04-06 01:01:06 +0300

    Add project files.

---
+++ src/Core/CorePackages.Domain/Comman/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace CorePackages.Domain.Comman;
+
+public class BaseEntity
+{
+    public Guid Id { get; set; }
+    public DateTime CreateDate { get; set; }
+}
---
+++ src/Core/CorePackages.Domain/Entities/Configuration.cs
@@ -0,0 +1,35 @@
+﻿using CorePackages.Domain.Comman;
+
+namespace CorePackages.Domain.Entities;
+
+public class Configuration<T> : BaseEntity
+{
+    public string Name { get; set; }
+    // Type alanını T tipine göre otomatik olarak belirlemek için
+    public string Type
+    {
+        get
+        {
+            return GetTypeString(typeof(T));
+        }
+    }
+    public T Value { get; set; }
+    public bool IsActive { get; set; }
+    public string ApplicationName { get; set; }
+
+
+
+    private string GetTypeString(Type type)
+    {
+        if (type == typeof(int))
+            return "int";
+        else if (type == typeof(string))
+            return "string";
+        else if (type == typeof(double))
+            return "double";
+        else if (type == typeof(bool))
+            return "bool";
+        else
+            return "unknown";
+    }
+}
