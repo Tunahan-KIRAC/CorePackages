68fd930637bbe3cb39bc8b42cd9ad29946677b94
Tunahan <Feleket@DESKTOP-E6PUBRK>
2024-04-07 04:49:50 +0300

    AddDefault

--- src/WebApi/CorePackages.WebApi/Controllers/ConfigurationController.cs
+++ src/WebApi/CorePackages.WebApi/Controllers/ConfigurationController.cs
@@ -5,6 +5,7 @@ using CorePackages.Application.Interfaces.Repository;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace CorePackages.WebApi.Controllers
 {
@@ -35,6 +36,45 @@ namespace CorePackages.WebApi.Controllers
             return Ok(await mediator.Send(command));
         }
 
+
+        [HttpPost]
+        public async Task<IActionResult> AddDefault()
+        {
+            var configurations = new List<CreateConfiguration>
+        {
+            new CreateConfiguration
+            {
+                Name = "SiteName",
+                Type = "string",
+                Value = "soty.io",
+                IsActive = true,
+                ApplicationName = "SERVICE-A"
+            },
+            new CreateConfiguration
+            {
+                Name = "IsBasketEnabled",
+                Type = "bool",
+                Value = "1",
+                IsActive = true,
+                ApplicationName = "SERVICE-B"
+            },
+            new CreateConfiguration
+            {
+                Name = "MaxItemCount",
+                Type = "Int",
+                Value = "50",
+                IsActive = false,
+                ApplicationName = "SERVICE-A"
+            }
+        };
+
+            foreach (var configuration in configurations)
+            {
+                await mediator.Send(configuration);
+            }
+
+            return Ok("Default configurations added successfully.");
+        }
 
 
         [HttpGet("{name}")]
